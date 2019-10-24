c36b74d3573c3792437870129d815d64b3051504
James Holloway-Currie <james.holloway-currie@beamdog.com>
2019-10-24 09:08:45 -0600

    Init Commit

---
+++ Source/MergeTest/MergeTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MergeTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MERGETEST_API AMergeTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
